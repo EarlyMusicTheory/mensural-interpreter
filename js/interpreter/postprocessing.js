a0029c9cdb9079586774dc1c78c44ba2a3779bc7
Anna Plaksin <Anna.Plaksin@bcu.ac.uk>
2021-07-02 17:00:50 +0200

    Add interpreter info to header

--- js/interpreter/postprocessing.js
+++ js/interpreter/postprocessing.js
@@ -50,6 +50,100 @@ var post = (function() {
         }
     }
 
+    /**
+     * Adds application info to MEI file
+     * <encodingDesc>
+            <appInfo>
+                <application xml:id="mensural-interpreter">
+                    <name>Mensural interpreter</name>
+                </application>
+            </appInfo>
+        </encodingDesc>
+     * @param {MEIdoc} meiDoc 
+     * @memberof post
+     * @inner
+     */
+    function addAppInfo (meiDoc){
+        // if there is no encodingDesc, add one behind fileDesc
+        var meiHead = meiDoc.doXPathOnDoc("//mei:meiHead", meiDoc.doc, 9).singleNodeValue;
+        var encodingDesc;
+
+        if(meiHead.getElementsByTagName("encodingDesc").length===0)
+        {
+            let fileDesc = meiHead.getElementsByTagName("fileDesc")[0];
+            encodingDesc = meiDoc.addMeiElement("encodingDesc");
+            fileDesc.after(encodingDesc);
+        }
+        else
+        {
+            encodingDesc = meiHead.getElementsByTagName("encodingDesc")[0];
+        }
+
+        // if there is no appInfo, add it after head (or as first child)
+        var appInfo = encodingDesc.getElementsByTagName("appInfo")[0];
+
+        if(!appInfo)
+        {
+            appInfo = meiDoc.addMeiElement("appInfo");
+            let head = encodingDesc.getElementsByTagName("head")[0];
+            if(head)
+            {
+                head.after(appInfo);
+            }
+            else
+            {
+                encodingDesc.prepend(appInfo);
+            }
+        }
+
+        // add application as last child of appInfo
+        var app = meiDoc.addMeiElement("application");
+        app.setAttribute("xml:id", "mensural-interpreter");
+        appInfo.append(app);
+        let name = meiDoc.addMeiElement("name");
+        name.textContent = "Mensural interpreter";
+        app.append(name);
+    }
+
+    /**
+     * Add revisionDesc entry for interpreter
+     * <revisionDesc>
+            <change isodate="2021" resp="#mensural-interpreter">
+                <changeDesc>
+                    <p>Resolved mensural durations.</p>
+                </changeDesc>
+            </change>
+        </revisionDesc>
+     * @param {MEIdoc} meiDoc 
+     * @memberof post
+     * @inner
+     */
+    function addRevisionDesc (meiDoc){
+        // if there is no revisionDesc, add one as last child of meiHead
+        var meiHead = meiDoc.doXPathOnDoc("//mei:meiHead", meiDoc.doc, 9).singleNodeValue;
+        var revisionDesc = meiHead.getElementsByTagName("revisionDesc")[0];
+
+        if(!revisionDesc)
+        {
+            revisionDesc = meiDoc.addMeiElement("revisionDesc");
+            meiHead.append(revisionDesc);
+        }
+
+        // add change as last child
+        var change = meiDoc.addMeiElement("change");
+        revisionDesc.append(change);
+        // add isodate and resp
+        change.setAttribute("resp", "#mensural-interpreter");
+        let date = new Date();
+        change.setAttribute("isodate", date.toISOString());
+        // add changeDesc and p
+        let p = meiDoc.addMeiElement("p");
+        p.textContent = "Resolved mensural durations.";
+        let changeDesc = meiDoc.addMeiElement("changeDesc");
+        changeDesc.append(p);
+        change.append(changeDesc);
+    }
+
     return{
         
         /**
@@ -59,6 +153,8 @@ var post = (function() {
          */
         run : function (meiDoc) {
             addBarLines(meiDoc);
+            addAppInfo(meiDoc);
+            addRevisionDesc(meiDoc);
         }
     }
 })();
